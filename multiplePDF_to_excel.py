91110beadb7c8c2e69f59cd1d6b7c460e132a48e
Satyajeet Kumar <156283001+HiveCase@users.noreply.github.com>
2025-03-01 23:34:23 +0530

    Create multiplePDF_to_excel.py

---
+++ multiplePDF_to_excel.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import pdfplumber
+import os
+
+# Get all PDF files in the directory
+directory_path = './'
+pdf_files = [f for f in os.listdir(directory_path) if f.endswith('.pdf')]
+
+dfs = []
+for filename in pdf_files:
+    file_path = os.path.join(directory_path, filename)
+    if os.path.exists(file_path):
+        with pdfplumber.open(file_path) as pdf:
+            for page in pdf.pages:
+                table = page.extract_table()
+                if table:  # Check if a table is found
+                    if not dfs:
+                        df = pd.DataFrame(table[1:], columns=table[0])  # Set the header row as column names
+                    else:
+                        df = pd.DataFrame(table[1:])  # Exclude the header row
+                    dfs.append(df)
+    else:
+        print(f"File not found: {file_path}")
+
+# Concatenate all DataFrames into a single one
+df_all = pd.concat(dfs, ignore_index=True)
+
+# Save the DataFrame to an Excel file
+output_directory = './processed_files/'
+os.makedirs(output_directory, exist_ok=True)
+df_all.to_excel(os.path.join(output_directory, 'all_tables.xlsx'), index=False)
